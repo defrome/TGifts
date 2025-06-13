62438fe4ee42eb0b4a3c662ea0abd5426df75b8e
defrome <iskoblov555@gmail.com>
2025-06-13 12:48:05 +0300

    change

--- main.py
+++ main.py
@@ -1,124 +1,58 @@
-from typing import AsyncGenerator
-import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, Update, WebAppInfo, LabeledPrice, PreCheckoutQuery
-from aiogram.filters import CommandStart
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+import os
+from dotenv import load_dotenv
 import uvicorn
-from starlette.middleware.cors import CORSMiddleware
-from config_reader import config
+from aiogram.types import LabeledPrice
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from aiogram.types import Update
+from fastapi import FastAPI
+from fastapi.requests import Request
+from starlette.responses import JSONResponse
+from aiogram import Router, Dispatcher, Bot, types
 
-# Инициализация бота и диспетчера
-bot = Bot(config.BOT_TOKEN.get_secret_value())
+# Bot initialization
+my_router = Router()
 dp = Dispatcher()
-
-# Создаем FastAPI приложение
 app = FastAPI()
 
-# Настройка CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+load_dotenv()  # Загружаем переменные из .env
 
-# Клавиатура с WebApp кнопкой
-markup = (
-    InlineKeyboardBuilder()
-    .button(text="Open app", web_app=WebAppInfo(url=config.WEBAPP_URL))
-).as_markup()
+bot = Bot(token=os.getenv("BOT_TOKEN"))
 
+@app.get('/payment')
+async def create_invoice_link_bot():
+   payment_link = await bot.create_invoice_link(
+       "Case",
+       "25 stars",
+       "{}",
+       "XTR",
+       prices=[LabeledPrice(label="Кейс с подарками", amount=25)]
+   )
+   return payment_link
 
-# Обработчики для бота
-@dp.message(CommandStart())
-async def start(message: Message) -> None:
-    await message.answer("Hello!", reply_markup=markup)
+# Lifespan manager for FastAPI app
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    url_webhook = "https://localhost:8000/webhook"
+    await bot.set_webhook(url=url_webhook,
+                          allowed_updates=dp.resolve_used_update_types(),
+                          drop_pending_updates=True)
+    yield
+    await bot.delete_webhook()
 
+# Accepting payments
+@my_router.pre_checkout_query(lambda query: True)
+async def pre_checkout_query(pre_checkout_q: types.PreCheckoutQuery):
+    await bot.answer_pre_checkout_query(pre_checkout_q.id, ok=True)
 
-@dp.pre_checkout_query()
-async def precheck(event: PreCheckoutQuery) -> None:
-    await event.answer(True)
-
-
-@dp.message(F.successful_payment)
-async def successful_payment(message: Message) -> None:
-    await message.answer("Ваши звезды зачислены на ваш баланс")
-
-
-# Обработчики для FastAPI
-@app.post(config.WEBHOOK_PATH)
-async def bot_webhook(request: Request) -> None:
-    update = Update.model_validate(await request.json(), context={'bot': bot})
+@app.post("/webhook")
+async def webhook(request: Request):
+    new_update_msg = await request.json()
+    successful_payment = new_update_msg.get("message", {}).get("successful_payment")
+    if successful_payment:
+       return JSONResponse ({'response': successful_payment})
+    update = Update.model_validate(new_update_msg, context={"bot": bot})
     await dp.feed_update(bot, update)
 
-
-@app.post("/api/donate")
-async def donate(request: Request) -> JSONResponse:
-    data = await request.json()
-    invoice_link = await bot.create_invoice_link(
-        title="Пополнение баланса",
-        description="Пополнение игрового баланса",
-        payload="Оплата",
-        provider_token=config.PAYMENT_TOKEN.get_secret_value(),
-        currency="RUB",
-        prices=[LabeledPrice(label="Игровая валюта", amount=data["amount"] * 100)]  # amount в копейках
-    )
-    return JSONResponse({"invoice_link": invoice_link})
-
-
-async def run_bot():
-    await bot.delete_webhook(drop_pending_updates=True)
-    try:
-        await dp.start_polling(bot)
-    except asyncio.CancelledError:
-        await bot.session.close()
-
-
-def run_fastapi():
-    uvicorn.run(
-        app,
-        host=config.APP_HOST,
-        port=config.APP_PORT,
-        log_level="info"
-    )
-
-
-async def main():
-    # Создаем и запускаем задачи
-    bot_task = asyncio.create_task(run_bot())
-    fastapi_task = asyncio.get_event_loop().run_in_executor(None, run_fastapi)
-
-    try:
-        # Ожидаем завершения одной из задач
-        done, pending = await asyncio.wait(
-            [bot_task, fastapi_task],
-            return_when=asyncio.FIRST_COMPLETED
-        )
-
-        # Отменяем оставшиеся задачи
-        for task in pending:
-            task.cancel()
-            try:
-                await task
-            except asyncio.CancelledError:
-                pass
-
-    except KeyboardInterrupt:
-        # Обработка Ctrl+C
-        bot_task.cancel()
-        try:
-            await bot_task
-        except asyncio.CancelledError:
-            pass
-        await bot.session.close()
-
-
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Приложение остановлено")
+    uvicorn.run(app, host="localhost", port=8000)
