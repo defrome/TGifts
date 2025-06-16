35c12963eb769cc6b89c63c4670c2a5f69e078f7
defrome <iskoblov555@gmail.com>
2025-06-17 00:32:11 +0300

    changes

--- main.py
+++ main.py
@@ -47,6 +47,19 @@ app.add_middleware(
 # Хранилища данных
 paid_users = {}
 user_inventory = {}
+gifts = [
+    {"telegram_id": "5170145012310081615", "gift_id": "1"},
+    {"telegram_id": "5170233102089322756", "gift_id": "2"},
+    {"telegram_id": "5170250947678437525", "gift_id": "3"},
+    {"telegram_id": "5168103777563050263", "gift_id": "4"},
+    {"telegram_id": "5170144170496491616", "gift_id": "5"},
+    {"telegram_id": "5170314324215857265", "gift_id": "6"},
+    {"telegram_id": "5170564780938756245", "gift_id": "7"},
+    {"telegram_id": "5168043875654172773", "gift_id": "8"},
+    {"telegram_id": "5170690322832818290", "gift_id": "9"},
+    {"telegram_id": "5170521118301225164", "gift_id": "10"},
+    {"telegram_id": "6028601630662853006", "gift_id": "11"}
+]
 
 
 async def init_user(user_id: int):
@@ -90,6 +103,7 @@ async def create_invoice_link_bot():
     logger.info("Создана ссылка на оплату")
     return {"invoice_link": payment_link}
 
+# Выдача подарка
 @app.get("/sendgift")
 async def send_telegram_gift(gift_id: str, user_id: int):
 
@@ -99,6 +113,7 @@ async def send_telegram_gift(gift_id: str, user_id: int):
         MessageEntity(type="bold", offset=0, length=2),  # Жирный смайлик 🎁
         MessageEntity(type="italic", offset=3, length=8)  # Курсив "подарок"
     ]
+
 
     try:
         # Отправка подарка
