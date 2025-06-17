31a0023705ca72a8c90d49017543dcb54d70aa09
defrome <iskoblov555@gmail.com>
2025-06-18 00:20:40 +0300

    массив с спин гифт

--- shared.py
+++ shared.py
@@ -15,6 +15,19 @@ gifts = [
     {"telegram_id": "6028601630662853006", "gift_id": "11", "emoji": "🍾", "image_path": "https://i.ibb.co/9mpvjS0m/shampane.png", "star": 50}
 ]
 
+spin_gifts = [
+    {"id": "5170145012310081615", "img": "https://i.ibb.co/83crdxt/heart.png", "name": "heart", "value": "15", "rarity": "rare", "probability": "21.37"},
+    {"id": "5170233102089322756", "img": "https://i.ibb.co/MyYTrssS/bear.png", "name": "bear", "value": "15", "rarity": "common", "probability": "25.81"},
+    {"id": "5170250947678437525", "img": "https://i.ibb.co/9HbdKTST/gift.png", "name": "gift", "value": "25", "rarity": "rare", "probability": "20.81"},
+    {"id": "5168103777563050263", "img": "https://i.ibb.co/tMrm9G4F/rose.png", "name": "rose", "value": "25", "rarity": "rare", "probability": "25"},
+    {"id": "5170144170496491616", "img": "https://i.ibb.co/h1Tqg4T9/cake.png", "name": "cake", "value": "50", "rarity": "epic", "probability": "10"},
+    {"id": "5170314324215857265", "img": "https://i.ibb.co/zTqgSHWt/flowers.png", "name": "flowers", "value": "50", "rarity": "epic", "probability": "10"},
+    {"id": "5170564780938756245", "img": "https://i.ibb.co/MDzMjStr/rocket.png", "name": "rocket", "value": "50", "rarity": "epic", "probability": "10"},
+    {"id": "5168043875654172773", "img": "https://i.ibb.co/Rk3kQxB2/cup.png", "name": "cup", "value": "100", "rarity": "legendarity", "probability": "0.8"},
+    {"id": "5170690322832818290", "img": "https://i.ibb.co/9mdyLZb2/ring.png", "name": "ring", "value": "100", "rarity": "epic", "probability": "5"},
+    {"id": "5170521118301225164", "img": "https://i.ibb.co/Zz9pX6cP/brilliant.png", "name": "brilliant", "value": "100", "rarity": "epic", "probability": "5"},
+    {"id": "6028601630662853006", "img": "https://i.ibb.co/9mpvjS0m/shampane.png", "name": "shampane", "value": "50", "rarity": "epic", "probability": "1.21"}
+]
 
 async def init_user(user_id: int):
     if user_id not in user_inventory:
