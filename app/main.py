aeb14bce3445497ca68d405aec1adc5cbac2bf2f
TraftG <taxmasib78@gmail.com>
2025-06-20 21:45:28 +0000

    add import os

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import uvicorn
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from bot.bot import bot, dp
 from bot.handlers import command_start_handler, on_pre_checkout, on_message, check_payment_status, process_refund
 from app.api import app as fastapi_app, app
