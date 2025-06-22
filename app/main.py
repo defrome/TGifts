7442556f47d1ace03f6533d0cb97deb0f19a7489
defrome <iskoblov555@gmail.com>
2025-06-22 13:27:45 +0300

    22 june

--- app/main.py
+++ app/main.py
@@ -4,23 +4,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 import logging
-from bot.bot import bot, dp
-from bot import handlers
+
 logger = logging.getLogger(__name__)
-
-# Создаем основной экземпляр FastAPI
-app = FastAPI()
-
-# Настройки CORS
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @asynccontextmanager
@@ -30,17 +15,37 @@ async def lifespan(app: FastAPI):
     logger.info("👋 Завершение работы")
 
 
-async def start_bot():  # Ленивый импорт
+def create_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan, title="My API", version="1.0")
+
+    # Настройки CORS
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return app
+
+
+app = create_app()
+
+
+async def start_bot():
+    from bot.bot import bot, dp
     await dp.start_polling(bot)
 
 
 async def start_fastapi():
-    from app.api import router as api_router  # Импортируем роутер, а не app
+    from app.api import router as api_router
 
-    # Включаем роутеры в основное приложение
+    # Подключаем роутер
     app.include_router(api_router, prefix="/api", tags=["API"])
 
-    config = uvicorn.Config(app, host="localhost", port=8000)
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000)
     server = uvicorn.Server(config)
     await server.serve()
 
